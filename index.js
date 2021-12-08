49494cd8e0b057a60294470f6ccb73dc37b04e96
Daniel <danbapp@gmail.com>
2021-12-08 13:47:55 -0500

    commit tree fb507993a508bfb10d9f1ae9722dcf5f72d69f2b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+// Create a record keeping application, used for time cards
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  let testEmployee = {
+    firstName: firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  }
+  return testEmployee;
+}
+
+function createEmployeeRecords() {
+    let twoRows = [
+      ["moe", "sizlak", "barkeep", 2],
+      ["bartholomew", "simpson", "scamp", 3]
+      ]
+  }
